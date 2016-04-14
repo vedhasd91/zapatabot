43bcdd166f137b0536e596feea51ff14cea7759f
Vedhas Deshpande <de.vedhas@gmail.com>
2016-04-14 19:27:06 -0400

    Reconfigured pins

--- GPS_interface_with_Xbee/GPS_interface_with_Xbee.ino
+++ GPS_interface_with_Xbee/GPS_interface_with_Xbee.ino
@@ -9,7 +9,7 @@
    It requires the use of SoftwareSerial, and assumes that you have a
    4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
 */
-static const int RXPin = 2, TXPin = 3;
+static const int RXPin = 3, TXPin = 2;
 static const int RXPin1 = 6, TXPin1 = 5;
 static const uint32_t GPSBaud = 4800;
 static const uint32_t ZbBaud = 9600;
